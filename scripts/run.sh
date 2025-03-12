852d9099914c0458be6294df07579f213342d16d
salamence637 <67583252+salamence637@users.noreply.github.com>
2025-03-12 10:28:51 +0900

    1

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 # Run docker compose
 docker compose -f docker-compose.prod.yml down --remove-orphans
-docker compose -f docker-compose.prod.yml build --no-cache
+# docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml up -d
 
 
